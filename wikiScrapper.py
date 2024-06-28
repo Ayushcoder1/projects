cfc4f5254bcd666560bf887e49462ea51f0228f1
Ayushcoder1 <115918633+Ayushcoder1@users.noreply.github.com>
2024-06-28 12:11:39 +0530

    Add files via upload

---
+++ wikiScrapper.py
@@ -0,0 +1,49 @@
+import requests, sys, webbrowser, bs4
+import re
+import string
+search = input('Enter the topic :- ')
+search = search.replace(' ','_')
+
+res = requests.get(f'https://en.wikipedia.org/wiki/{search}')
+res.raise_for_status()
+
+print('===========\n')
+text = re.findall('<p>(.*)?[.]',res.text)
+words = {}
+
+for item in text:
+    data = ''
+    item = item + '.'
+    index = 0
+    while index < len(item):
+        if item[index] != '<' and index < len(item) and item[index] != '&':
+            if item[index] != '>' and item[index] != ';':
+                data = data + item[index]
+                index += 1
+                continue
+            index += 1
+
+        if item[index] == '<' and index < len(item):
+            try:
+                index = item.index('>', index)
+            except:
+                index = len(item) - 1
+
+        if item[index] == '&' and index < len(item):
+            index = item.index(';', index)
+            try:
+                while item[index + 1].isdigit():
+                   index = item.index(';', index + 1)
+            except:
+                index = item.index(';', index)
+
+    for word in data.split():
+        words[word] = words.get(word,0) + 1
+
+    print(data)
+    print('==================' * 3)
+
+print('=================='*3)
+print('=================='*3)
+print('=================='*3)
+print(words)
